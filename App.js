4c2187344ca0e1193a8ba2d3ec742a11c822c492
mnemokaz <mnemokaz@gmail.com>
2023-11-08 00:08:29 +0200

    newTodo create

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
+import {React, useState} from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
 import {Navbar} from './src/Navbar';
+import {AddTodo} from './src/AddTodo';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = title => {
+    const newTodo = {
+      id: Date.now().toString(),
+      titile: title,
+    };
+  };
+
   return (
-    <View style={styles.container}>
+    <View>
       <Navbar title="Todo APP" />
       <StatusBar style="auto" />
+      <View style={styles.container}></View>
+      <AddTodo />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
 });
