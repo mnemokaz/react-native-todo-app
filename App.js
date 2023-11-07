a7c11e34a71a1b5de4c5ab2a5acc968630f709d3
mnemokaz <mnemokaz@gmail.com>
2023-11-07 23:23:29 +0200

    Navbar create

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {Navbar} from './src/Navbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
+      <Navbar title="Todo APP" />
       <StatusBar style="auto" />
     </View>
   );
