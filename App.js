5a8245666b0a30efb8bf059b0a181b5de9f92aa3
mnemokaz <mnemokaz@gmail.com>
2023-11-08 14:57:00 +0200

    input done

--- App.js
+++ App.js
@@ -3,23 +3,42 @@ import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
 import {Navbar} from './src/Navbar';
 import {AddTodo} from './src/AddTodo';
+import {Todo} from './src/Todo';
 
 export default function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = title => {
-    const newTodo = {
-      id: Date.now().toString(),
-      titile: title,
-    };
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   titile: title,
+    // };
+    //   setTodos(prevTodos => {
+    //     return [...prevTodos, newTodo];
+    //   });
+    // };
+
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title,
+      },
+    ]);
   };
 
   return (
     <View>
       <Navbar title="Todo APP" />
       <StatusBar style="auto" />
-      <View style={styles.container}></View>
-      <AddTodo />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+        <View>
+          {todos.map(todo => {
+            return <Todo todo={todo} key={todo.id} />;
+          })}
+        </View>
+      </View>
     </View>
   );
 }
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import {React, useState} from 'react';
 import {View, StyleSheet, TextInput, Button} from 'react-native';
 
-export const AddTodo = props => {
+export const AddTodo = ({onSubmit}) => {
+  const [value, setValue] = useState('');
+  const pressHandler = () => {
+    onSubmit(value);
+    setValue('');
+  };
+
   return (
     <View style={styles.block}>
-      <TextInput style={styles.input} />
-      <Button title="Добавить" />
+      <TextInput
+        style={styles.input}
+        onChangeText={setValue}
+        value={value}
+        placeholder="Введите название задачи"
+      />
+      <Button title="Добавить" onPress={pressHandler} />
     </View>
   );
 };
@@ -15,6 +26,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    marginBottom: 15,
   },
   input: {
     width: '70%',
